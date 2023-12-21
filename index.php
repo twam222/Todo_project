0de5faf03ebf8345c91cc811f3d493026d5fbf21
imad manouli <aimad.manouli.2003@gmail.com>
2023-12-21 23:33:04 +0000

    ajoute des constantes de la structure html dans index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    define('DB_USER', 'root');
+    define('DB_PASS', 'hmad_lok');
+    define('DB_NAME', 'todolist'); 
+    define('DB_HOST', '127.0.0.1'); 
+    define('DB_PORT', '3306');
+    
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
